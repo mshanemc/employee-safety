a7556a61e72cdfe20fb26ddb4cc73bedb57e028a
mshanemc <shane.mclaughlin@salesforce.com>
2019-02-18 17:29:47 -0600

    easier mobile logins

--- orgInit.sh
+++ orgInit.sh
@@ -1,3 +1,5 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s -o safe.ty --userprefix user -n
 sfdx force:source:push
 sfdx force:org:open
+# login for mobile
+sfdx shane:user:password:set -g User -l User
