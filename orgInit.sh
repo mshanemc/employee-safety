febc5445f91e87a08580ea0b776b58601b2c1f04
mshanemc <shane.mclaughlin@salesforce.com>
2019-02-19 07:56:56 -0600

    add permset part

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,6 @@
 sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s -o safe.ty --userprefix user -n
 sfdx force:source:push
+sfdx force:user:permset:assign -n Safety
 sfdx force:org:open
 # login for mobile
 sfdx shane:user:password:set -g User -l User
