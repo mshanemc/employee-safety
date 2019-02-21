93f11427e6087a1188c8a087fb2dc4599dd933e3
mshanemc <shane.mclaughlin@salesforce.com>
2019-02-21 11:00:46 -0800

    script fixes

--- orgInit.sh
+++ orgInit.sh
@@ -1,8 +1,8 @@
-sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s -o safe.ty --1prefix user -n
+sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s -o safe.ty --userprefix user -n
 sfdx force:source:push
 sfdx force:user:permset:assign -n Safety
 sfdx force:org:open
 # login for mobile
-sfdx shane:user:password:set -g User -l User
+sfdx shane:user:password:set -g User -l User -p sfdx1234
 sfdx force:data:bulk:upsert -f data/incident_reports.csv -s Incident_Report__c -i id
 sfdx force:data:bulk:upsert -f data/observations.csv -s Observation__c -i id
